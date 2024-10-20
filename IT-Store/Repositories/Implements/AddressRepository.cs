43f218b1d92ca4abd9de38b0e48f349f5f64f2d9
Aly <el7amrawy@outlook.com>
2024-10-20 16:14:30 +0300

    add address repository

---
+++ IT-Store/Repositories/Implements/AddressRepository.cs
@@ -0,0 +1,28 @@
+﻿using IT_Store.Models;
+using IT_Store.Repositories.Interfaces;
+
+namespace IT_Store.Repositories.Implements
+{
+    public class AddressRepository : Repository<Address>, IAddressRepository
+    {
+        private readonly CodexContext _db;
+
+        public AddressRepository(CodexContext db):base(db)
+        {
+            _db = db;
+        }
+
+        public override Address GetById(int id)
+        {
+            if (!IsExisted(id)) {
+                throw new Exception("Address does not exist");
+            }
+            return _db.Addresses.FirstOrDefault(a => a.Id == id);
+        }
+
+        public override bool IsExisted(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
